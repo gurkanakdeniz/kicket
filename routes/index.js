be3207b4a85a80a5cb1bc8049cb421d66a8b3708
ilkemerol <ilkemerol@hotmail.com>
2019-06-18 13:28:32 +0300

    initlzr

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname + "/../views/index.html"));
+  res.sendFile(path.join(__dirname + "/../views/test.html"));
 });
 
 module.exports = router;
