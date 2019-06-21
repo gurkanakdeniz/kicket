caf871f1a9fc87e1e169378b27e1f5a651ce14dc
gurkanakdeniz <akdenizgurkan@gmail.com>
2019-06-22 00:49:47 +0300

    modules, changes

--- models/api.model.js
+++ models/api.model.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
-const restApiSchema = mongoose.Schema({
+const apiSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   url: { type: String },
   platform: { type: String },
   creator: { type: String },
   usage: { type: Number, default: 0 },
+  usageIpHistory: { type: [String] },
   created_date: { type: Date, default: Date.now },
   whitelist: { type: String }
 });
 
-module.exports = mongoose.model("restApi", restApiSchema);
+module.exports = mongoose.model("api", apiSchema);
---
+++ services/run.utility.service.js
@@ -0,0 +1,35 @@
+exports.getApi = async function getApi(platform) {
+  if (platform === "node") {
+    return process.env.NODE_API + "/run/";
+  } else if (platform === "html") {
+    return process.env.HTML_API + "/run/";
+  } else if (platform === "java") {
+    return process.env.JAVA_API + "/run/";
+  } else if (platform === "python") {
+    return process.env.PYTHON_API + "/run/";
+  }
+};
+
+exports.getHeader = async function getHeader(platform) {
+  if (platform === "java") {
+    return { "Content-Type": "application/json" };
+  } else if (platform === "node") {
+    return { "Content-Type": "text/plain" };
+  } else if (platform === "html") {
+    return { "Content-Type": "text/plain" };
+  } else if (platform === "python") {
+    return { "Content-Type": "text/plain" };
+  }
+};
+
+exports.getBody = async function getBody(platform, data) {
+  if (platform === "java") {
+    return data;
+  } else if (platform === "node") {
+    return data;
+  } else if (platform === "html") {
+    return data;
+  } else if (platform === "python") {
+    return data;
+  }
+};
