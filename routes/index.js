5187ffbd7df2eebc774899d8430c46b305c68dc1
ilkemerol <ilkem.erol@hotmail.com>
2019-08-20 23:22:52 +0300

    footer & mobile device dev

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@ var path = require("path");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
+  var MobileDetect = require("mobile-detect"),
+    md = new MobileDetect(req.headers["user-agent"]);
+  if (md.mobile()) {
+    res.sendFile(path.join(__dirname + "/../views/mobile.html"));
+  } else {
+    res.sendFile(path.join(__dirname + "/../views/index.html"));
+  }
+});
+
+router.get("/:force", function(req, res, next) {
   res.sendFile(path.join(__dirname + "/../views/index.html"));
 });
 
--- styles/js/attach.js
+++ styles/js/attach.js
@@ -3,3 +3,5 @@ $("#deploy-load").load("../views/deployloading.html");
 $("#editor-load").load("../views/editorloading.html");
 
 $("#walkthrough-load").load("../views/walkthrough.html");
+
+$("#footer-load").load("../views/footer.html");
